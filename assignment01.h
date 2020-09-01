714462b3e12a4bd5ee9dde9877eba8bc5fea276d
JustinMcLain1 <70154173+JustinMcLain1@users.noreply.github.com>
2020-08-31 21:18:31 -0500

    Add files via upload

---
+++ assignment01.h
@@ -0,0 +1,16 @@
+#ifndef H_ASSIGNMENT01 // should not be defined any place else 
+#define H_ASSIGNMENT01 // same const value as for ifndef directive
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstdlib>
+#include<iomanip>
+
+// Add needed header files here ..
+
+// You need to implement these two functions ...
+// void genRndNums(vector<int> &v);
+// void printVec(const vector<int> &v);
+
+#endif
